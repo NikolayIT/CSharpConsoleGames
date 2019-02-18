8fd718efe7ee0d1164a381e3e8d2e790a6eda9ef
Nikolay Kostov <admin@nikolay.it>
2019-02-18 22:30:43 +0200

    .NET Core version of the Tetris game (Part 2)

--- Tetris (.NET Core)/Program.cs
+++ Tetris (.NET Core)/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace Tetris
@@ -11,9 +12,52 @@ namespace Tetris
         static int InfoCols = 10;
         static int ConsoleRows = 1 + TetrisRows + 1;
         static int ConsoleCols = 1 + TetrisCols + 1 + InfoCols + 1;
-
+        static List<bool[,]> TetrisFigures = new List<bool[,]>()
+            {
+                new bool[,] // ----
+                {
+                    { true, true, true, true }
+                },
+                new bool[,] // O
+                {
+                    { true, true },
+                    { true, true }
+                },
+                new bool[,] // T
+                {
+                  { false, true, false },
+                  { true, true, true },
+                },
+                new bool[,] // S
+                {
+                    { false, true, true, },
+                    { true, true, false, },
+                },
+                new bool[,] // Z
+                {
+                    { true, true, false },
+                    { false, true, true },
+                },
+                new bool[,] // J
+                {
+                    { true, false, false },
+                    { true, true, true }
+                },
+                new bool[,] // L
+                {
+                    { false, false, true },
+                    { true, true, true }
+                },
+            };
+        
         // State
         static int Score = 0;
+        static int Frame = 0;
+        static int FramesToMoveFigure = 15;
+        static int CurrentFigureIndex = 2;
+        static int CurrentFigureRow = 0;
+        static int CurrentFigureCol = 0;
+        static bool[,] TetrisField = new bool[TetrisRows, TetrisCols];
 
         static void Main(string[] args)
         {
@@ -25,8 +69,8 @@ namespace Tetris
             Console.BufferWidth = ConsoleCols;
             while (true)
             {
-                Score++;
-
+                Frame++;
+                // Read user input
                 if (Console.KeyAvailable)
                 {
                     var key = Console.ReadKey();
@@ -36,13 +80,47 @@ namespace Tetris
                         // Environment.Exit(0);
                         return; // Becase of Main()
                     }
+                    if (key.Key == ConsoleKey.LeftArrow || key.Key == ConsoleKey.A)
+                    {
+                        // TODO: Move current figure left
+                        CurrentFigureCol--; // TODO: Out of range
+                    }
+                    if (key.Key == ConsoleKey.RightArrow || key.Key == ConsoleKey.D)
+                    {
+                        // TODO: Move current figure right
+                        CurrentFigureCol++; // TODO: Out of range
+                    }
+                    if (key.Key == ConsoleKey.DownArrow || key.Key == ConsoleKey.S)
+                    {
+                        Frame = 1;
+                        Score++;
+                        CurrentFigureRow++;
+                        // TODO: Move current figure down
+                    }
+                    if (key.Key == ConsoleKey.Spacebar || key.Key == ConsoleKey.UpArrow || key.Key == ConsoleKey.W)
+                    {
+                        // TODO: Implement 90-degree rotation of the current figure
+                    }
                 }
-                // user input
-                // change state
+
+                // Update the game state
+                if (Frame % FramesToMoveFigure == 0)
+                {
+                    CurrentFigureRow++;
+                    Frame = 0;
+                }
+                // // TODO: if (Collision())
+                // {
+                //      AddCurrentFigureToTetrisField()
+                //      CheckForFullLines()
+                //      if (lines remove) Score++;
+                // }
 
                 // Redraw UI
                 DrawBorder();
                 DrawInfo();
+                // TODO: DrawTetrisField();
+                DrawCurrentFigure();
 
                 Thread.Sleep(40);
             }
@@ -85,6 +163,23 @@ namespace Tetris
         {
             Write("Score:", 1, 3 + TetrisCols);
             Write(Score.ToString(), 2, 3 + TetrisCols);
+            Write("Frame:", 4, 3 + TetrisCols);
+            Write(Frame.ToString(), 5, 3 + TetrisCols);
+        }
+
+        static void DrawCurrentFigure()
+        {
+            var currentFigure = TetrisFigures[CurrentFigureIndex];
+            for (int row = 0; row < currentFigure.GetLength(0); row++)
+            {
+                for (int col = 0; col < currentFigure.GetLength(1); col++)
+                {
+                    if (currentFigure[row, col])
+                    {
+                        Write("*", row + 1 + CurrentFigureRow, col + 1 + CurrentFigureCol);
+                    }
+                }
+            }
         }
 
         static void Write(string text, int row, int col, ConsoleColor color = ConsoleColor.Yellow)
