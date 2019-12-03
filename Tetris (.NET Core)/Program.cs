e6217711947e4475e67db6a60680d39997262882
rumenand <rumen80@abv.bg>
2019-12-03 22:46:57 +0200

    Removed Empty Spaces

--- Tetris (.NET Core)/Program.cs
+++ Tetris (.NET Core)/Program.cs
@@ -5,9 +5,9 @@ namespace Tetris
 {
     // TODO: When we have collision during moving (left/right) we have unusual behaviour
     public static class Program
-    {       
+    {
         public static void Main()
-        {           
+        {
             new MusicPlayer().Play();
             int TetrisRows = 20;
             int TetrisCols = 10;
